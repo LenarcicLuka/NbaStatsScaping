d1363ee674c9e94e14616ca1cc36855cd6207784
Luka <lukaalenarcic@gmail.com>
2020-11-13 03:37:21 +0100

    Final NbaStatsScraping version.

---
+++ Readme_NbaStatsScraping.md
@@ -0,0 +1,20 @@
+# NbaStatsScraping
+
+## Description
+NbaStatsScraping is a java program used for scraping a website: "https://nba.com/players" 
+It uses Selenium library with geckodriver(Firefox). 
+Program takes user input, which should be players full name and than prints out the 3-pointers average the player
+managed for each season data is available for, during the regular season, per 40 minutes.  
+
+## Requirements
+JDK
+Intellij(Gradle)
+
+## Installation
+Build and run the project.
+
+## Usage
+Write full name of a player and get to know what was his 3-pointers average, for
+each season data is available for, during the regular season, per 40 minutes. Because
+site has problems with loading statistics, there is an error message and an
+option to try your luck again.
--- src/main/java/com/nba/stats/NbaStatsScrape.java
+++ src/main/java/com/nba/stats/NbaStatsScrape.java
@@ -1,94 +1,102 @@
 package com.nba.stats;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Scanner;
 
 public class NbaStatsScrape {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static String playerName;
+
+    public static void main(String[] args) {
+
+        // new WebDriver, using headless=false for debug
+        FirefoxDriver driver = new FirefoxDriver(new FirefoxOptions().setHeadless(false));
+        // new WebDriverWait, used to wait for loading of elements
+        WebDriverWait wait = new WebDriverWait(driver, 12);
+
+        // loop while no result found, on error try again ('-1' for exit)
+        while (getPlayer()) {
+            try {
+                driver.navigate().to("https://nba.com/players"); // go to nba.com/players
+
+                // accept cookies...
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#onetrust-accept-btn-handler")));
+                WebElement btnAcpt = driver.findElementByCssSelector("#onetrust-accept-btn-handler");
+                btnAcpt.click();
+
+                // wait for dirty page reloading
+                Thread.sleep(3000);
+
+                // wait/find search bar and type players name from input
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@aria-label='Player Natural Search Bar']")));
+                WebElement playerSearchInput = driver.findElementByXPath("//input[@aria-label='Player Natural Search Bar']");
+                playerSearchInput.sendKeys(playerName);
+
+                // keep waiting for only 1 player left in the result table -> if input data does not guarantee it, we're lost, commenting out
+//                List<WebElement> players;
+//                do { players = driver.findElements(By.cssSelector("[class='players-list'] tr")); } while (players.size() != 2);
+                Thread.sleep(2000);
+
+                // find first player element in the table and click it
+                WebElement firstPlayer = driver.findElementByXPath("//a[@class='flex items-center t6']");
+                firstPlayer.click();
+
+                // find players stats link, click it
+                WebElement statsPlayer = driver.findElementByXPath("//a[@class='flex px-2 border-b-4 h-full justify-center items-center hover:no-underline hover:border-black focus:border-cerulean focus:outline-none border-transparent']");
+                statsPlayer.click();
+
+                // wait for dirty page reloads again
+                Thread.sleep(3000);
+
+                // wait/find selector for period, select 40 min option
+                wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("[name='PerMode']")));
+                Select gameMode = new Select(driver.findElementByCssSelector("[name='PerMode']"));
+                gameMode.selectByVisibleText("Per 40 Minutes");
+
+                // wait/find selector for season type, select regular season option
+                wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("[name='SeasonType']")));
+                Select seasonMode = new Select(driver.findElementByCssSelector("[name='SeasonType']"));
+                seasonMode.selectByVisibleText("Regular Season");
 
 
-        // Scanner Scan = new Scanner(System.in);
-        // System.out.println("Enter player name: ");
+                // wait and select players stats table
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[template='player/player-traditional']")));
+                WebElement table = driver.findElement(By.cssSelector("[template='player/player-traditional']"));
 
-        // String imeIgralca = Scan.nextLine();
+                Thread.sleep(1500);
 
+                // loop table rows, print year and 3PA cell values
+                for (WebElement row : table.findElements(By.tagName("tr"))) {
+                    List<WebElement> rowElements = row.findElements(By.tagName("td"));
+                    String year = rowElements.get(0).getText();
+                    String threePA = rowElements.get(9).getText();
+                    System.out.println("Year:" + year + " " + "3PA: " + threePA);
+                }
 
+                break; // got result, exiting loop
 
-        FirefoxDriver driver;
-
-        final FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setHeadless(false);
-        driver = new FirefoxDriver(firefoxOptions);
-
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-
-        driver.navigate().to("https://nba.com/players");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#onetrust-accept-btn-handler")));
-
-        WebElement btnAcpt = driver.findElementByCssSelector("#onetrust-accept-btn-handler");
-
-        btnAcpt.click();
-
-        Thread.sleep(3000);
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@aria-label='Player Natural Search Bar']")));
-
-        WebElement playerSearchInput = driver.findElementByXPath("//input[@aria-label='Player Natural Search Bar']");
-
-        //playerSearchInput.sendKeys(imeIgralca);
-        playerSearchInput.sendKeys("dragic");
-
-        List<WebElement> players;
-
-        do {
-            players = driver.findElements(By.cssSelector("[class='players-list'] tr"));
-        } while (players.size() != 2);
-
-
-        WebElement firstPlayer = driver.findElementByXPath("//a[@class='flex items-center t6']");
-        firstPlayer.click();
-
-        WebElement statsPlayer = driver.findElementByXPath("//a[@class='flex px-2 border-b-4 h-full justify-center items-center hover:no-underline hover:border-black focus:border-cerulean focus:outline-none border-transparent']");
-        statsPlayer.click();
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[name='PerMode']")));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[name='SeasonType']")));
-
-        Thread.sleep(3000);
-
-        Select gameMode = new Select(driver.findElementByCssSelector("[name='PerMode']"));
-        gameMode.selectByVisibleText("Per 40 Minutes");
-
-        Select seasonMode = new Select(driver.findElementByCssSelector("[name='SeasonType']"));
-        seasonMode.selectByVisibleText("Regular Season");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[template='player/player-traditional']")));
-
-        WebElement table = driver.findElement(By.cssSelector("[template='player/player-traditional']"));
-        for (WebElement element : table.findElements(By.tagName("tr"))) {
-            List<WebElement> rowElements = element.findElements(By.tagName("td"));
-            String year = rowElements.get(0).getText();
-            String threePA = rowElements.get(9).getText();
-            System.out.println("ROW: " + element.getText());
-            System.out.println("Year:" + year + " " + "3PA: " + threePA);
+            } catch (Exception e) {
+                System.out.println("There was and error try to find the players stats, you can try again."); // on error try again
+                e.printStackTrace();
+            }
         }
+        driver.close(); // done deal, close driver
+    }
 
-
-//        driver.close();
+    // get standard input for player name; if '-1' return false to exit while loop
+    private static boolean getPlayer() {
+        System.out.println("Enter players name ('-1' for exit): ");
+        playerName = scanner.nextLine();
+        return !playerName.equals("-1");
     }
 
 }
